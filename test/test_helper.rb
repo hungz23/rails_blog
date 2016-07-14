372a668ea56dc7e634fba40565bc6628b261910d
hungz23 <hungtranbuddha@gmail.com>
2016-07-14 16:12:01 +0700

    add session for login logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,11 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  	fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+	def is_logged_in?
+		!session[:user_id].nil?
+	end
 end
