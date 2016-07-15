cf094a483cfd3bb6b6928a01f31d90285e66cf46
hungz23 <hungtranbuddha@gmail.com>
2016-07-15 15:29:12 +0700

    Update Gemfile.lock for Heroku

--- Gemfile
+++ Gemfile
@@ -44,6 +44,11 @@ group :development, :test do
   gem 'byebug'
 end
 
+group :production do
+	gem 'pg', '0.17.1'
+	gem 'rails_12factor', '0.0.2'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+                                        :following, :followers, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def index
